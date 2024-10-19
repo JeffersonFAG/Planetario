9b826531d01aaf03bf961618433bf1352219f701
Jefferson Almanza <jefferson.almanza@litethinking.com>
2024-10-19 10:57:17 -0500

    fix accessibility

--- src/components/SortSelect/SortSelect.tsx
+++ src/components/SortSelect/SortSelect.tsx
@@ -8,14 +8,18 @@ interface SortSelectProps {
 
 export const SortSelect: React.FC<SortSelectProps> = ({ order, onChange }) => {
   return (
-    <select
-      aria-label="select order"
-      value={order}
-      onChange={(e) => onChange(e.target.value)}
-      className="border rounded p-2 text-black"
-    >
-      <option value="asc">Ordenar A-Z</option>
-      <option value="desc">Ordenar Z-A</option>
-    </select>
+    <>
+      <label htmlFor="select-order"></label>
+      <select
+        id="select-order"
+        aria-label="select order"
+        value={order}
+        onChange={(e) => onChange(e.target.value)}
+        className="border rounded p-2 text-black"
+      >
+        <option value="asc">Ordenar A-Z</option>
+        <option value="desc">Ordenar Z-A</option>
+      </select>
+    </>
   );
 };
