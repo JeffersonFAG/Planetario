7177136ba663145c9aafb03c7d2b155bd1e459da
Jefferson Almanza <jefferson.almanza@litethinking.com>
2024-10-19 10:52:58 -0500

    fix accessibility

--- src/pages/planets/index.tsx
+++ src/pages/planets/index.tsx
@@ -44,43 +44,40 @@ const PlanetsPage = () => {
   );
 
   return (
-    <main>
-      {" "}
-      <div className="bg-custom-bg bg-cover bg-center flex flex-col items-center w-full">
-        {/* Added flexbox for centering */}
-        <h1
-          aria-label="title-page"
-          className="text-4xl md:text-5xl lg:text-6xl text-center font-extrabold mt-12 mb-16 text-gray-800"
-        >
-          Listado de Planetas
-        </h1>
-        <div
-          aria-label="section search"
-          className="flex flex-row items-center gap-4"
-        >
-          <SearchInput value={queryset} onChange={updateQueryset} />
-          <SortSelect order={order} onChange={updateOrder} />
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-4">
-          {displayedPlanets.length > 0 ? (
-            displayedPlanets.map((planet) => (
-              <PlanetCard key={planet.id} planet={planet} />
-            ))
-          ) : (
-            <div className="flex justify-center items-center w-full col-span-full my-4">
-              <span className="text-lg text-gray-700 text-4xl">
-                Una galaxia de conocimiento te espera
-              </span>
-            </div>
-          )}
-        </div>
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={setCurrentPage}
-        />
+    <div className="bg-custom-bg bg-cover bg-center flex flex-col items-center w-full">
+      {/* Added flexbox for centering */}
+      <h1
+        aria-label="title-page"
+        className="text-4xl md:text-5xl lg:text-6xl text-center font-extrabold mt-12 mb-16 text-gray-800"
+      >
+        Listado de Planetas
+      </h1>
+      <div
+        aria-label="section search"
+        className="flex flex-row items-center gap-4"
+      >
+        <SearchInput value={queryset} onChange={updateQueryset} />
+        <SortSelect order={order} onChange={updateOrder} />
       </div>
-    </main>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-4">
+        {displayedPlanets.length > 0 ? (
+          displayedPlanets.map((planet) => (
+            <PlanetCard key={planet.id} planet={planet} />
+          ))
+        ) : (
+          <div className="flex justify-center items-center w-full col-span-full my-4">
+            <span className="text-lg text-gray-700 text-4xl">
+              Una galaxia de conocimiento te espera
+            </span>
+          </div>
+        )}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
+    </div>
   );
 };
 
